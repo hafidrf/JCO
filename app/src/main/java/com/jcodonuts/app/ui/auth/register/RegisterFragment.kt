7b90837b74ad43feb7df1dec7b21e34b21946481
Choirul Muhtadin <choimuhtadin@gmail.com>
2021-01-27 17:07:54 +0700

    sms autofill

--- app/src/main/java/com/jcodonuts/app/ui/auth/register/RegisterFragment.kt
+++ app/src/main/java/com/jcodonuts/app/ui/auth/register/RegisterFragment.kt
@@ -1,7 +1,14 @@
 package com.jcodonuts.app.ui.auth.register
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.credentials.Credential
+import com.google.android.gms.auth.api.credentials.HintRequest
+import com.google.android.gms.auth.api.phone.SmsRetriever
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -11,9 +18,12 @@ import com.jcodonuts.app.databinding.FragmentRegisterBinding
 import com.jcodonuts.app.ui.base.BaseFragment
 import com.jcodonuts.app.utils.DialogJco
 import com.jcodonuts.app.utils.KeyboardUtil
+import com.jcodonuts.app.utils.sms.AppSignatureHelper
+import com.jcodonuts.app.utils.sms.MySMSBroadcastReceiver
+import com.jcodonuts.app.utils.sms.OtpListener
 import javax.inject.Inject
 
-class RegisterFragment @Inject constructor() : BaseFragment<FragmentRegisterBinding, RegisterViewModel>() {
+class RegisterFragment @Inject constructor() : BaseFragment<FragmentRegisterBinding, RegisterViewModel>(), OtpListener {
     private val TAG = "LoginFragment"
 
     private lateinit var auth: FirebaseAuth
@@ -31,12 +41,15 @@ class RegisterFragment @Inject constructor() : BaseFragment<FragmentRegisterBind
         KeyboardUtil(requireActivity(), binding.root)
         auth = Firebase.auth(FirebaseApp.initializeApp(requireContext())!!)
         initObserver()
+        initSMSReceiver()
+        binding.ccp.registerCarrierNumberEditText(binding.edtPhoneNo.editText)
         binding.btnBack.setOnClickListener {
             onBackPress()
         }
 
         binding.btnNext.setOnClickListener {
-            viewModel.verifyPhoneNumber(binding.edtPhoneNo.editText?.text.toString(), auth, requireActivity())
+            Log.d("DATA__", binding.ccp.fullNumberWithPlus)
+            viewModel.verifyPhoneNumber(binding.ccp.fullNumberWithPlus, auth, requireActivity())
         }
 
         binding.btnVerification.setOnClickListener {
@@ -70,5 +83,21 @@ class RegisterFragment @Inject constructor() : BaseFragment<FragmentRegisterBind
                 binding.lytOTP.visibility = View.VISIBLE
             }
         })
+    }
+
+    private fun initSMSReceiver() {
+        val client = SmsRetriever.getClient(requireActivity())
+        val task = client.startSmsRetriever()
+        task.addOnSuccessListener {
+            Log.i(TAG, "SMS RETRIEVER TASK SUCCESSFULLY ADDED")
+        }
+        task.addOnFailureListener {
+            Log.i(TAG, "SMS RETRIEVER TASK FAILED")
+        }
+        MySMSBroadcastReceiver.otpListener = this
+    }
+
+    override fun onOtpReceived(otp: String) {
+        binding.otpView.setText(otp)
     }
 }
---
+++ app/src/main/java/com/jcodonuts/app/utils/sms/Utils.kt
@@ -0,0 +1,54 @@
+package com.jcodonuts.app.utils.sms
+
+import android.app.Activity
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+fun Int.isNumeric(): Boolean {
+    if (this == KeyEvent.KEYCODE_0
+        || this == KeyEvent.KEYCODE_1
+        || this == KeyEvent.KEYCODE_2
+        || this == KeyEvent.KEYCODE_3
+        || this == KeyEvent.KEYCODE_4
+        || this == KeyEvent.KEYCODE_5
+        || this == KeyEvent.KEYCODE_6
+        || this == KeyEvent.KEYCODE_7
+        || this == KeyEvent.KEYCODE_8
+        || this == KeyEvent.KEYCODE_9
+    ) {
+        return true
+    }
+    return false
+}
+
+object Utils {
+    fun hideKeyboard(activity: Activity) {
+        val imm: InputMethodManager =
+            activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        //Find the currently focused view, so we can grab the correct window token from it.
+        var view: View? = activity.currentFocus
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = View(activity)
+        }
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    fun getOtp(sms: String): String {
+        var isDigitFound = false
+        val stringArr = sms.split("").toTypedArray()
+        val builder = StringBuilder()
+        for (string in stringArr) {
+            if (string.matches(Regex("[0-9]+"))) {
+                builder.append(string)
+                isDigitFound = true
+            } else {
+                if (isDigitFound) {
+                    return builder.toString()
+                }
+            }
+        }
+        return ""
+    }
+}
